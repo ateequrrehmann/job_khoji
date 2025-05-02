a44ac7ad53665082071f04d9979296d305dbed73
Huzaifa UR Rehman <rehmanateequr501@gmail.com>
2025-05-02 22:54:03 +0500

    model/database schemas

---
+++ backend/models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose, { mongo } from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phoneNumber: {
+        type: number,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['student', 'recruiter'],
+        required: true,
+    },
+    profile: {
+        bio: {
+            type: String,
+        },
+        skills: [{ type: String }],
+        resume: {
+            type: String, //url
+        },
+        resumeOriginalName: { type: String },
+        company: {
+            type: mongoose.Schema.Types.ObjectId,  //company for which it apply
+            ref: 'Company',
+        },
+        profilePhone: {
+            type: String,
+            default: '',
+        }
+    },
+}, { timestamps: true });
+
+export const User = mongoose.model('User', userSchema);
