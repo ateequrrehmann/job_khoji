5271e95f93890ee8d4af9e1e7aa20f08ed6fa22b
Huzaifa UR Rehman <rehmanateequr501@gmail.com>
2025-05-02 20:26:35 +0500

    initializing the basic server

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';   //we access the data in of frontend in backend
+import cors from 'cors';   //cross-origin resource sharing
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: 'http://localhost:5173',  //for frontend
+    credentials: true
+}
+
+app.use(cors(corsOptions));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
